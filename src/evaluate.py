714eb9a4eeedcb8505c2f0a6fcf6b10ab3e03127
ash-mcc <ashley_mcclenaghan@mac.com>
2022-09-16 06:59:50 +0100

    rotate feat imp plot

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,11 +2,13 @@
 
 from sklearn import metrics
 import pandas as pd
+import numpy as np
 import pyarrow.feather as feather
 import pickle
 import json
 import os
 import sys
+import time
 from dvclive import Live
 from matplotlib import pyplot as plt
 
@@ -43,12 +45,19 @@ print('X_test {}, y_test {}'.format(X_test.shape, y_test.shape))
 
 mean_accuracy =  model.score(X_test, y_test)
 
+
 y_hat_proba = model.predict_proba(X_test)
+
 print('y_hat_proba {} {}'.format(y_hat_proba.shape, y_hat_proba[:5]))
 
 y_hat = model.predict(X_test)
 print('y_hat {} {}'.format(y_hat.shape, y_hat))
 
+
+cl_rpt = metrics.classification_report(y_test, y_hat, output_dict=True)
+
+# use the MCC when a binary classifier
+# sklearn.metrics.matthews_corrcoef(y_true, y_pred, *, sample_weight=None)
 
 
 print('Writing metrics to file')
@@ -56,20 +65,29 @@ print('Writing metrics to file')
 with open(metrics_filepath, 'w') as outfile:
     json.dump(
         {"train":
-         {"mean_accuracy": mean_accuracy}},
+         {"mean_accuracy": mean_accuracy,
+          "weighted_avg_f1_score": cl_rpt["weighted avg"]["f1-score"]}},
         outfile)
 
-fig, axes = plt.subplots(dpi=100)
-fig.subplots_adjust(bottom=0.2, top=0.95)
+
+
+start_time = time.time()
 importances = model.feature_importances_
+std = np.std([tree.feature_importances_ for tree in model.estimators_], axis=0)
 forest_importances = pd.Series(importances, index=X_cols) #.nlargest(n=30)
-axes.set_ylabel("Importance")
-forest_importances.plot.bar(ax=axes)
+elapsed_time = time.time() - start_time
+print(f"Elapsed time to compute the importances: {elapsed_time:.3f} seconds")
+
+fig, ax = plt.subplots(dpi=100)
+fig.subplots_adjust(left=0.2)
+ax.set_xlabel("Mean decrease in impurity")
+#ax.set_title("Feature importances")
+forest_importances.plot.barh(xerr=std, ax=ax, align="center")
 fig.savefig(importance_filepath)
 
 
 live = Live(live_dir)
-# live.log_plot("roc", y_test, y_hat)
+# live.log_plot("roc", y_test, y_hat)#
 # live.log("avg_prec", metrics.average_precision_score(y_test, y_hat))
 # live.log("roc_auc", metrics.roc_auc_score(y_test, y_hat))
 live.log_plot("confusion_matrix", y_test.squeeze(), y_hat_proba.argmax(-1))
