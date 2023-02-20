38dd1189ca0fcb45828da4c0ba3ca76805a8bc7d
ash-mcc <ashley_mcclenaghan@mac.com>
2023-02-20 15:10:58 +0000

    finished this revision

--- src/params.py
+++ src/params.py
@@ -1,6 +1,6 @@
 random_forest = {
     'n_estimators': 100,
-    'criterion': 'gini',
+    'criterion': 'gini', # gini, entropy, log_loss
     'class_weight': 'balanced_subsample',
     'n_jobs': -1,
     'random_state': 42,
